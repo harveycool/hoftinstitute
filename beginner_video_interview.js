a9971f492e01e608f0d4a9da2c66cd17831d1d57
Harvey Xanthos <harveyxanthos@gmail.com>
2024-08-05 13:52:01 -0500

    changed endpoint url

--- beginner_video_interview.js
+++ beginner_video_interview.js
@@ -14,7 +14,9 @@ AWS.config.update({
   accessKeyId: "DO00YZE99PML9HXFV7JV",
   secretAccessKey: "G3Qp8YArA3Sb9i1VFDhxATVcqV524NuZHBoZFVhsxmU",
 });
-const spacesEndpoint = new AWS.Endpoint("nyc3.digitaloceanspaces.com");
+const spacesEndpoint = new AWS.Endpoint(
+  "https://hoftfiles.nyc3.digitaloceanspaces.com"
+);
 const s3 = new AWS.S3({
   endpoint: spacesEndpoint,
 });
